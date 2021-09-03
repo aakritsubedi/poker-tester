95abc29f4320124b5e9297fec74bb9b72e5e563f
aakritsubedi <aakritsubedi9@gmail.com>
2021-09-03 12:38:09 +0545

    Add modal to view summary after auto run

--- src/components/AutoTester.js
+++ src/components/AutoTester.js
@@ -4,10 +4,18 @@ import React, { useEffect, useState } from "react";
 
 import PokerService from "../services/poker";
 
+import { Controlled as CodeMirror } from "react-codemirror2";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/neat.css";
+require("codemirror/mode/css/css");
+require("codemirror/mode/javascript/javascript");
+
 const AutoTester = () => {
   const [socket, setSocket] = useState("");
   const [selectedServer, setSelectedServer] = useState("");
   const [servers, setServers] = useState([]);
+
+  const [selectedEvent, setSelectedEvent] = useState("");
 
   const [events, setEvents] = useState([]);
 
@@ -114,88 +122,161 @@ const AutoTester = () => {
     }
   }, [selectedServer]);
 
+  const viewModal = (event) => {
+    setSelectedEvent(event);
+    console.log(selectedEvent);
+  };
+
   return (
-    <div className="container">
-      <Select
-        value={selectedServer}
-        options={servers}
-        onChange={changeServer}
-      />
-      {isConnected.status ? (
-        <span className="help-text success">{isConnected.message}</span>
-      ) : (
-        <span className="help-text danger">{isConnected.message}</span>
-      )}
+    <>
+      <div className="container">
+        <Select
+          value={selectedServer}
+          options={servers}
+          onChange={changeServer}
+        />
+        {isConnected.status ? (
+          <span className="help-text success">{isConnected.message}</span>
+        ) : (
+          <span className="help-text danger">{isConnected.message}</span>
+        )}
 
-      {isConnected.status ? (
-        <div className="status">
-          <div className="dot dot-success"></div>
-          <span>Online</span>
-        </div>
-      ) : (
-        <div className="status">
-          <div className="dot dot-danger"></div>
-          <span>Offline</span>
-        </div>
-      )}
-      <br />
-
-      <div className="alert alert-primary clearfix">
-        <span id="success-summary">
-          {events.filter((event) => event.status === "Success").length}/
-          {
-            events.filter((event) => event.status !== "Insufficient data")
-              .length
-          }{" "}
-          successful.
-        </span>
+        {isConnected.status ? (
+          <div className="status">
+            <div className="dot dot-success"></div>
+            <span>Online</span>
+          </div>
+        ) : (
+          <div className="status">
+            <div className="dot dot-danger"></div>
+            <span>Offline</span>
+          </div>
+        )}
         <br />
-        {events.filter((event) => event.status === "Insufficient data").length}/
-        {events.length} have insufficient data.
-        <button
-          className="btn btn-success float-right"
-          onClick={triggerAllEvents}
-        >
-          Trigger All
-        </button>
+
+        <div className="alert alert-primary clearfix">
+          <span id="success-summary">
+            {events.filter((event) => event.status === "Success").length}/
+            {
+              events.filter((event) => event.status !== "Insufficient data")
+                .length
+            }{" "}
+            successful.
+          </span>
+          <br />
+          {
+            events.filter((event) => event.status === "Insufficient data")
+              .length
+          }
+          /{events.length} have insufficient data.
+          <button
+            className="btn btn-success float-right"
+            onClick={triggerAllEvents}
+          >
+            Trigger All
+          </button>
+        </div>
+        <div className="data-summary"></div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>SN</th>
+              <th>Socket Info</th>
+              <th>Status</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {events.map((row, index) => {
+              return (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>
+                    <b>Event: </b> {row.event} <br />
+                    <b>Listener: </b> {row.listener}
+                  </td>
+                  <td>{row.status}</td>
+                  <td>
+                    {row.data && (
+                      <button
+                        className="btn btn-primary mr-2"
+                        onClick={() =>
+                          fireEvent(row.event, row.data, row.listener, index)
+                        }
+                      >
+                        Run
+                      </button>
+                    )}
+                    {row.status === "Success" && (
+                      <button
+                        className="btn btn-primary"
+                        data-toggle="modal"
+                        data-target="#eventInfo"
+                        onClick={() => viewModal(row)}
+                      >
+                        View
+                      </button>
+                    )}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
-      <div className="data-summary"></div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>SN</th>
-            <th>Socket Info</th>
-            <th>Status</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {events.map((row, index) => {
-            return (
-              <tr key={index}>
-                <td>{index + 1}</td>
-                <td>
-                  <b>Event: </b> {row.event} <br />
-                  <b>Listener: </b> {row.listener}
-                </td>
-                <td>{row.status}</td>
-                <td>
-                  {row.data && (
-                    <button
-                      onClick={() =>
-                        fireEvent(row.event, row.data, row.listener, index)
-                      }
-                    >
-                      Run
-                    </button>
-                  )}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+      {selectedEvent && (
+        <div
+          className="modal fade"
+          id="eventInfo"
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="eventInfoLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="eventInfoLabel">
+                  Summary of event `{selectedEvent.event}`
+                </h5>
+                <button
+                  type="button"
+                  className="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                <div>
+                  <table className="table table-sm">
+                    <tr>
+                      <td>Event Name: {selectedEvent.event}</td>
+                    </tr>
+                    <tr>
+                      <td>Event Listener: {selectedEvent.listener}</td>
+                    </tr>
+                    <tr>
+                      <td>Status: {selectedEvent.status}</td>
+                    </tr>
+                  </table>
+                </div>
+                <h3>Response</h3>
+                <CodeMirror
+                  value={JSON.stringify(selectedEvent.response, null, 2)}
+                  options={{
+                    mode: "javascript",
+                    theme: "material",
+                    lineNumbers: true,
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
